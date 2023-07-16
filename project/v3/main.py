df356b46876eb22427b7a6f7cf8d925713b9e60e
Lee Seungwoo <115550749+LswLlama@users.noreply.github.com>
2023-07-16 18:08:32 +0900

    Update main.py

--- project/v3/main.py
+++ project/v3/main.py
@@ -34,7 +34,7 @@ def process_frame(frame):
     roi_vertices = np.array([[
         (0, height),
         (width // 2 - 50, height // 2 + 50),
-        (width // 2 + 50, height // 2 + 50),
+        (width, height // 2 + 50),
         (width, height)
     ]], dtype=np.int32)
     roi_mask = np.zeros_like(mask)
@@ -44,7 +44,7 @@ def process_frame(frame):
     # 허프 변환을 사용하여 직선 검출
     lines = cv2.HoughLinesP(masked_image, 1, np.pi / 180, 130, minLineLength=100, maxLineGap=50)
 
-    slope = 0  # 초기화
+    angle = 0  # 초기화
     intersection_x = 0  # 초기화
 
     if lines is not None:
@@ -60,7 +60,7 @@ def process_frame(frame):
                     # 80도에서 100도인 직선 무시
                     angle_threshold = 10  # 각도 기준 값
                     angle = np.degrees(np.arctan(slope))
-                    if angle >= 80 and angle <= 100:
+                    if (angle >= 80 and angle <= 100) and (angle <10 and angle > -10):
                         continue
 
                     # 직선이 화면 하단과 만나는 지점의 x 좌표 계산
@@ -69,13 +69,12 @@ def process_frame(frame):
                     cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
                 # 파이시리얼 통신 (현장에서 angle, distance_to_right_line 변수 확인 및 튜닝 필요)
-                angle = np.degrees(np.arctan(slope))
                 intersection_x = int(x1 + (height - y1) * (x2 - x1) / (y2 - y1))
                 py_serial(str(angle), str(intersection_x))
-                return frame, slope, intersection_x
+                return frame, angle, intersection_x
 
     # lines가 None인 경우에는 기본 값을 반환
-    return frame, slope, intersection_x
+    return frame, angle, intersection_x
 
 
 # 동영상 또는 카메라 입력을 사용하여 프레임을 읽어옴
@@ -87,9 +86,9 @@ while True:
     if not ret:
         break
 
-    processed_frame, slope, intersection_x = process_frame(frame)
+    processed_frame, angle, intersection_x = process_frame(frame)
 
-    cv2.putText(processed_frame, f"Slope: {slope:.2f}, Intersection: ({intersection_x}, {frame.shape[0]})", (10, 30),
+    cv2.putText(processed_frame, f"Angle: {angle:.2f}, Intersection: ({intersection_x}, {frame.shape[0]})", (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow("Lane Detection", processed_frame)
