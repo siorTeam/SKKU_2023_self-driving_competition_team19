17a746775387eeee4cf70cf554a1cb8fc731972f
Lee Seungwoo <115550749+LswLlama@users.noreply.github.com>
2023-07-17 22:59:33 +0900

    Update v3.ino

--- project/v3/v3.ino
+++ project/v3/v3.ino
@@ -1,21 +1,17 @@
 //최적화 버전
 #include <PID_v1.h>
-
 // Pin
 const int potentiometerPin = A8;  // 가변저항 연결 핀
 const int motor_f_1 = 2;
 const int motor_f_2 = 3;
-int motor_b_l_1 = 4; 
-int motor_b_l_2 = 5; 
-int motor_b_r_1 = 7; 
-int motor_b_r_2 = 6; 
-
-//속도 
+int motor_b_l_1 = 4;
+int motor_b_l_2 = 5;
+int motor_b_r_1 = 7;
+int motor_b_r_2 = 6;
+//속도
 int Deceleration_threshold = 20;
 int speed;
 int speed_offset = 255;
-
-
 // PID
 // 앞바퀴
 double setpoint;  // 목표값(튜닝 완료)
@@ -26,42 +22,36 @@ const double consKd = 0.125;
 const double aggKp = 10;  // 공격적인 제어에 사용할 하이퍼 파라미터
 const double aggKi = 0.0075;
 const double aggKd = 0.6;
-const double A_chng_max = 25; // (튜닝 필요)
+const double A_chng_max = 30; // (튜닝 필요) (30+40:40은 오류 발생 이슈)
 const double W_chng_max = 40; // (튜닝 필요)
-const double chng_max = 25;
-
+const double chng_max = 30;
 // 폭에 의한 제어값 변화량 (튜닝 필요)
-double W_setpoint = 520;  // 이상적인 차폭(offset)
+double W_setpoint = 530;  // 이상적인 차폭(offset)
 double W_pid_input, W_chng_output;     // 차선과의 폭 PID 제어에 사용할 변수
 const double W_consKp = 1.4;  // 공격적인 제어에 사용할 하이퍼 파라미터: 가까이 있을 때
 const double W_consKi = 0.01;
 const double W_consKd = 0.1;
-
-// 폭에 의한 제어값 변화량 (튜닝 필요)
-double A_setpoint = 53.13;  // 이상적인 차폭 (offset) arctan (1.43)
+// 각도에 의한 제어값 변화량 (튜닝 필요)
+double A_setpoint = 60;  // 이상적인 차폭 (offset)
 double A_pid_input, A_chng_output;     // 차선과의 폭 PID 제어에 사용할 변수
-const double A_consKp = 1.5;  // 공격적인 제어에 사용할 하이퍼 파라미터: 가까이 있을 때
+const double A_consKp = 2;  // 공격적인 제어에 사용할 하이퍼 파라미터: 가까이 있을 때
 const double A_consKi = 0.01;
 const double A_consKd = 0.1;
-
 double CarLine_W, CarLine_A;
 double chng_sum;
 const double N_setpoint = 570;
-
 PID pid(&pid_input, &pid_output, &setpoint, consKp, consKi, consKd, DIRECT);  // PID 객체 생성
 PID W_pid(&W_pid_input, &W_chng_output, &W_setpoint, W_consKp, W_consKi, W_consKd, DIRECT);  // 차선과 폭 제어 PID 객체 생성
 PID A_pid(&A_pid_input, &A_chng_output, &A_setpoint, A_consKp, A_consKi, A_consKd, DIRECT);  // 차선과 각도 PID 객체 생성
-
 void setup() {
   // 모터 설정
   pinMode(motor_f_1, OUTPUT);
   pinMode(motor_f_2, OUTPUT);
-  pinMode(motor_b_l_1, OUTPUT); 
-  pinMode(motor_b_l_2, OUTPUT); 
-  pinMode(motor_b_r_1, OUTPUT); 
-  pinMode(motor_b_r_2, OUTPUT); 
+  pinMode(motor_b_l_1, OUTPUT);
+  pinMode(motor_b_l_2, OUTPUT);
+  pinMode(motor_b_r_1, OUTPUT);
+  pinMode(motor_b_r_2, OUTPUT);
   Serial.begin(9600);
-
   // PID 제어 설정
   pid.SetMode(AUTOMATIC);
   pid.SetOutputLimits(-255, 255);  // 출력 제한 설정
@@ -70,29 +60,29 @@ void setup() {
   A_pid.SetMode(AUTOMATIC);
   A_pid.SetOutputLimits(-A_chng_max, A_chng_max);  // 출력 제한 설정
 }
-
 void loop() {
   if (Serial.available()) {
     String receivedString = Serial.readStringUntil('\n');
     int commaIndex = receivedString.indexOf(',');
     String line_angle_input_str = receivedString.substring(0, commaIndex);
     String width_input_str = receivedString.substring(commaIndex + 1);
-
     CarLine_A = line_angle_input_str.toFloat();
     CarLine_W = width_input_str.toFloat();
-
     // #### 폭 차에 의한 PID 제어
     W_pid_input = CarLine_W;
     W_pid.SetTunings(W_consKp, W_consKi, W_consKd);
     W_pid.Compute();
-
     // #### 각도 차에 의한 PID 제어
     A_pid_input = CarLine_A;
     A_pid.SetTunings(A_consKp, A_consKi, A_consKd);
     A_pid.Compute();
-
     // #### 핸들 PID 제어
-    if (not W_pid_input and not A_pid_input){ //제어 인풋 없을 경우 직진
+    //제어 인풋 없을 경우 직진
+    if (not W_pid_input and not A_pid_input){
+      setpoint = N_setpoint;
+    }
+    // C 구간 극복 (값이 이상하게 받아질 경우 직진)
+    else if (W_pid_input >= 3000){
       setpoint = N_setpoint;
     }
     else{
@@ -101,8 +91,6 @@ void loop() {
       pid_input = analogRead(potentiometerPin);
       setpoint = N_setpoint + chng_sum;
     }
-
-
     double gap = abs(setpoint - pid_input);
     if (gap > 8) {
       pid.SetTunings(consKp, consKi, consKd);
@@ -110,7 +98,6 @@ void loop() {
       pid.SetTunings(aggKp, aggKi, aggKd);
     }
     pid.Compute();
-
     // 앞바퀴 DC 모터 회전 제어
     if (pid_output > 0) {
       analogWrite(motor_f_1, pid_output);
@@ -133,31 +120,28 @@ void loop() {
       analogWrite(motor_f_2, 0);
     }
     //급회전시 감속
-    if (abs(pid_output) >= 20){
-      speed = speed_offset/3;
+    if (abs(pid_output) >= chng_max - 1){
+      speed = speed_offset/1.8;
     } else {
-      speed = speed_offset/2;
+      speed = speed_offset/1.5;
     }
-
     // 뒷바퀴
-    analogWrite(motor_b_l_1, speed); 
+    analogWrite(motor_b_l_1, speed);
     analogWrite(motor_b_l_2, 0);
-    analogWrite(motor_b_r_1, speed); 
+    analogWrite(motor_b_r_1, speed);
     analogWrite(motor_b_r_2, 0);
-
     //##### 디버깅 정보 출력 #####//
     // 체인지 값을  빼니까 -면 좌회전
     // Serial.println("potentiometerValue: " + String(potentiometerValue));
-    Serial.println("A_pid_input: " + String(A_pid_input));
+    // Serial.println("A_pid_input: " + String(A_pid_input));
     Serial.println("A_chng_output: " + String(A_chng_output));
-    Serial.println("W_pid_input: " + String(W_pid_input));
+    // Serial.println("W_pid_input: " + String(W_pid_input));
     Serial.println("W_chng_output: " + String(W_chng_output));
     // Serial.println("chng_sum: " + String(chng_sum));
     // Serial.println("setpoint: " + String(setpoint));
-    // Serial.println("pid_output: " + String(pid_output));
-    // Serial.println();
-    delay(50);  // 딜레이
+    Serial.println("pid_output: " + String(pid_output));
+    Serial.println();
+    // delay(50);  // 딜레이
     //##### 디버깅 정보 출력 #####//
-
   }
 }
