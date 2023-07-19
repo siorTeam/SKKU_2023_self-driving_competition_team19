1b5c65ea04601abcdb0497065de211db051a930f
Kim Doyeop <138211749+K-Doyeop@users.noreply.github.com>
2023-07-19 18:45:56 +0900

    Update avoid.py

--- project/exercise/avoid.py
+++ project/exercise/avoid.py
@@ -22,6 +22,21 @@ def py_serial(line_angle_input, width_input):
         response = arduino.readline().decode().strip()
         print(response)
 
+# Function to check for obstacle within 1 meter
+def check_obstacle():
+    obstacle_detected = False
+
+    # Code to read data from the two ultrasonic sensors (you need to implement this part)
+    # For example:
+    # left_distance = read_distance_from_left_sensor()
+    # right_distance = read_distance_from_right_sensor()
+
+    # Assuming you have left_distance and right_distance values in meters
+    if left_distance <= 1.0 or right_distance <= 1.0:
+        obstacle_detected = True
+
+    return obstacle_detected
+
 def process_frame(frame):
     # 이미지 전처리
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -81,7 +96,6 @@ def process_frame(frame):
     # lines가 None인 경우에는 기본 값을 반환
     return frame, angle, intersection_x
 
-
 # 동영상 또는 카메라 입력을 사용하여 프레임을 읽어옴
 video_capture = cv2.VideoCapture(0)
 display_interval = 50  # 값을 출력하는 주기 (300 밀리초로 설정)
@@ -91,65 +105,33 @@ while True:
     if not ret:
         break
 
-    processed_frame, angle, intersection_x = process_frame(frame)
+    # Check for obstacle
+    obstacle_detected = check_obstacle()
 
-    cv2.putText(processed_frame, f"Angle: {angle:.2f}, Intersection: ({intersection_x}, {frame.shape[0]})", (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    if obstacle_detected:
+        # Stop the car
+        py_serial("0", "0")  # Sending "0" values to stop the car
 
-    cv2.imshow("Lane Detection", processed_frame)
+        # Avoidance behavior
+        time.sleep(1)  # Wait for 1 second
+        py_serial("-90", "100")  # Turn left for 4 seconds (assuming you have appropriate motor control logic in Arduino)
+        time.sleep(4)
+
+        py_serial("90", "100")  # Turn right for 4 seconds
+        time.sleep(4)
+
+    else:
+        # Continue normal operation
+        processed_frame, angle, intersection_x = process_frame(frame)
+
+        cv2.putText(processed_frame, f"Angle: {angle:.2f}, Intersection: ({intersection_x}, {frame.shape[0]})", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        cv2.imshow("Lane Detection", processed_frame)
 
     # 프레임 처리 속도를 조절하기 위해 대기 시간 지정
     if cv2.waitKey(display_interval) & 0xFF == ord('q'):
         break
-
-# 장애물 회피기동
-import cv2
-import numpy as np
-import time
-import serial
-
-# 파이 시리얼 포트 설정 및 연결 대기
-arduino = serial.Serial('COM8', 9600)
-time.sleep(2)
-
-# ... (기존 코드)
-
-def avoid_obstacle():
-    # 회피 동작
-    print("Avoiding obstacle...")
-    arduino.write(b"STOP\n")  # 아두이노로 "STOP" 메시지 전송
-
-    # 왼쪽으로 4초간 이동
-    arduino.write(b"LEFT\n")
-    time.sleep(4)
-    arduino.write(b"STOP\n")
-
-    # 오른쪽으로 4초간 이동
-    arduino.write(b"RIGHT\n")
-    time.sleep(4)
-    arduino.write(b"STOP\n")
-
-# ... (기존 코드)
-
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        break
-
-    # ... (기존 코드)
-
-    # 초음파 센서 값 가져오기
-    ultrasonic1_value =  # 왼쪽 초음파 센서 값 가져오기 (코드에서 측정 방법에 따라서 다르게 작성해야 함)
-    ultrasonic2_value =  # 오른쪽 초음파 센서 값 가져오기 (코드에서 측정 방법에 따라서 다르게 작성해야 함)
-
-    # 전방에 장애물이 1미터 이내에 있는지 확인
-    if ultrasonic1_value < 100 or ultrasonic2_value < 100:
-        avoid_obstacle()
-
-    # ... (기존 코드)
-
-# ... (기존 코드)
-
 
 video_capture.release()
 cv2.destroyAllWindows()
